021124f613c5950e33ed4bdf1fb7018fe54ecc18
Steffen Winde <inserv@winde-ganzig.de>
2018-02-05 16:02:18 +0100

    bug

--- translation/de/swgrundpreis_de_lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$sLangName = 'German';
-
-$aLang = array(
-    'charset'                     => 'UTF-8',
-
-    'WEIGHT'                      => 'Gewichts-/Inhaltsangabe',
-);
---
+++ translations/de/swgrundpreis_de_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+$sLangName = 'German';
+
+$aLang = array(
+    'charset'               => 'UTF-8',
+
+    'WEIGHT'                => 'Gewichts-/Inhaltsangabe',
+    'BASICPRICE'			=> 'Grundpreis',
+);
