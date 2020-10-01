0d2dbca260e711c344936f529bd7db46fdb961e0
inserv@winde-ganzig.de <mnisw1966gh>
2020-10-01 13:01:02 +0200

    edit text DETAILS_ARTWEIGHT

--- translations/de/swgrundpreis_de_lang.php
+++ translations/de/swgrundpreis_de_lang.php
@@ -4,6 +4,6 @@ $sLangName = 'German';
 
 $aLang = array(
     'charset'                           => 'UTF-8',
-
+    'DETAILS_ARTWEIGHT'                 => 'Füllmenge',
     'BASICPRICE'			            => 'Grundpreis',
 );
