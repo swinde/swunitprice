c78b744abb264a21e77de91f5a12ddec934d0ee0
Steffen Winde <inserv@winde-ganzig.de>
2022-02-17 15:18:17 +0100

    edit übersetzung

--- translations/de/swgrundpreis_de_lang.php
+++ translations/de/swgrundpreis_de_lang.php
@@ -6,7 +6,8 @@ $aLang = array(
     'charset'                           => 'UTF-8',
     'DETAILS_ARTWEIGHT'                 => 'Gewicht',
     'BASICPRICE'			            => 'Grundpreis',
-    'G'                                 =>'g',
-    'KG'                                =>'kg',
+    'G'                                 =>  'g',
+    'KG'                                =>  'kg',
+    'ML'                                =>  'ml'
 
 );
