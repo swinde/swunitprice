a547d74a7781128c34c17d58bfb87c19b890012a
Steffen Winde <inserv@winde-ganzig.de>
2022-02-17 15:19:19 +0100

    version

--- metadata.php
+++ metadata.php
@@ -23,7 +23,7 @@ $aModule = [
         'de' => 'Grundpreis für Artikel. (V6]',
         'en' => 'Grundpreis für Artikel. (V6]',
     ],
-    'version'      => '1.2.7',
+    'version'      => '1.2.8',
     'author'       => 'Steffen Winde',
     'url'          => 'http://winde-ganzig.de/',
     'email'        => '',
